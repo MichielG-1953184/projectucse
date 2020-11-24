c042470b82e994b178a6d699717b77b6caa651c1
MichielGuilliams <michiel1998@gmail.com>
2020-11-24 15:24:51 +0100

    login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/login">Login</router-link> |
       <router-link to="/overview">Overview</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,9 @@
 <template>
   <div class="login">
+    <label>Username:</label>
+    <input type="email"/>
+    <label>Password:</label>
+    <input type="Password"/>
+    <router-link to="/overview" tag="button">Login</router-link>
   </div>
 </template>
