e7d8cf9fce0e23219939bf217b42ef842905cf12
MichielGuilliams <michiel1998@gmail.com>
2020-11-28 17:23:44 +0100

    add

--- src/views/AddForm.vue
+++ src/views/AddForm.vue
@@ -6,29 +6,86 @@
       <md-step id="first" md-label="Team" md-description="Optional" :md-done.sync="first">
         <b-row>
           <b-col>
-          <md-autocomplete v-model="searchNameCriteria" :md-options="$parent.accounts.map(accounts=>(accounts.email)).filter(email => email!=('dpo@uhasselt.be'))" md-layout="box" md-dense>
-              <label>Search</label>
-          </md-autocomplete>
+            <md-autocomplete v-model="searchNameCriteria" v-on:md-selected="addToList" :md-options="accounts" md-layout="box" md-dense>
+                <label>Search</label>
+            </md-autocomplete>
           </b-col>
           <b-col>
+            <label>Team members</label>
+            <b-list-group style="width:100%">
+              <b-list-group-item v-for="member in teamMembers" :key="member">{{member}}</b-list-group-item>
+            </b-list-group>
           </b-col>
         </b-row>
-
         <md-button class="md-raised md-primary" @click="setDone('first', 'second')">Continue</md-button>
       </md-step>
 
       <md-step id="second" md-label="Project Information" :md-error="secondStepError" :md-done.sync="second">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+        <b-row>
+          <label>Description:</label>
+          <md-field>
+          <md-input v-model="Description"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>Type agreement:</label>
+          <md-field>
+          <md-input v-model="Typeagreement"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>Begin date:</label>
+          <md-field>
+          <md-input v-model="Begindate"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>End date:</label>
+          <md-field>
+          <md-input v-model="Enddate"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>No date reason:</label>
+          <md-field>
+          <md-input v-model="Nodatereason"></md-input>
+          </md-field>
+        </b-row>
         <md-button class="md-raised md-primary" @click="setDone('second', 'third')">Continue</md-button>
         <md-button class="md-raised md-primary" @click="setError()">Set error!</md-button>
       </md-step>
 
       <md-step id="third" md-label="Questions" :md-done.sync="third">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+        <b-row>
+          <label>Description:</label>
+          <md-field>
+          <md-input v-model="Description"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>Type agreement:</label>
+          <md-field>
+          <md-input v-model="Typeagreement"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>Begin date:</label>
+          <md-field>
+          <md-input v-model="Begindate"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>End date:</label>
+          <md-field>
+          <md-input v-model="Enddate"></md-input>
+          </md-field>
+        </b-row>
+        <b-row>
+          <label>No date reason:</label>
+          <md-field>
+          <md-input v-model="Nodatereason"></md-input>
+          </md-field>
+        </b-row>
         <md-button class="md-raised md-primary" @click="setDone('third')">Done</md-button>
       </md-step>
     </md-steppers>
@@ -38,14 +95,23 @@
 
 <script>
   export default {
-    name: 'StepperLinear',
-    data: () => ({
+    name: 'Addform',
+  data() {
+    return{
+      Description:"",
+      Typeagreement:"",
+      Begindate:"",
+      Enddate:"",
+      Nodatereason:"",
+      searchNameCriteria:"",
+      accounts: this.$parent.accounts.map(accounts=>(accounts.email)).filter(email => email!=('dpo@uhasselt.be')),
+      teamMembers: [],
       active: 'first',
       first: false,
       second: false,
       third: false,
       secondStepError: null
-    }),
+    }},
     methods: {
       setDone (id, index) {
         this[id] = true
@@ -58,6 +124,11 @@
       },
       setError () {
         this.secondStepError = 'This is an error!'
+      },
+      addToList: function(event){
+        console.log(event)
+        this.teamMembers.push(event)
+
       }
     }
   }
