06252edd5f954c5d8f409dc9ae7dab3d8d1beba5
steffenlen <steffen.lenaerts@gmail.com>
2020-12-15 17:13:23 +0100

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
     chainWebpack: config => {
         config.module.rules.delete('eslint');
     }
+    
 }
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/projectucse/'
+      : '/'
+  }
